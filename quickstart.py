5f78d05d1158928e4452079d9360167437771e2e
GhoulBoii <github@ghoulcloud.slmail.me>
2024-05-20 13:53:36 +0530

    style(): indentation

--- quickstart.py
+++ quickstart.py
@@ -70,43 +70,40 @@ def receive_messages(message_no):
   result = service.users().messages().list(maxResults=message_no, userId='me').execute()
   messages = result.get('messages')
   for msg in messages:
-      # Get the message from its id
-      txt = service.users().messages().get(userId='me', id=msg['id']).execute()
+    # Get the message from its id
+    txt = service.users().messages().get(userId='me', id=msg['id']).execute()
+    try:
+        # Get value of 'payload' from dictionary 'txt'
+        payload = txt['payload']
+        headers = payload['headers']
 
+        # Look for Subject and Sender Email in the headers
+        for d in headers:
+            if d['name'] == 'Subject':
+                subject = d['value']
+            if d['name'] == 'From':
+                sender = d['value']
 
-      # Use try-except to avoid any Errors
-      try:
-          # Get value of 'payload' from dictionary 'txt'
-          payload = txt['payload']
-          headers = payload['headers']
+        # The Body of the message is in Encrypted format. So, we have to decode it.
+        # Get the data and decode it with base 64 decoder.
+        parts = payload.get('parts')[0]
+        data = parts['body']['data']
+        data = data.replace("-","+").replace("_","/")
+        decoded_data = base64.b64decode(data)
 
-          # Look for Subject and Sender Email in the headers
-          for d in headers:
-              if d['name'] == 'Subject':
-                  subject = d['value']
-              if d['name'] == 'From':
-                  sender = d['value']
+        # Now, the data obtained is in lxml. So, we will parse
+        # it with BeautifulSoup library
+        soup = BeautifulSoup(decoded_data , "lxml")
+        body = soup.body()
 
-          # The Body of the message is in Encrypted format. So, we have to decode it.
-          # Get the data and decode it with base 64 decoder.
-          parts = payload.get('parts')[0]
-          data = parts['body']['data']
-          data = data.replace("-","+").replace("_","/")
-          decoded_data = base64.b64decode(data)
+        # Printing the subject, sender's email and message
+        print("\033[1;33;40mSubject: ", subject)
+        print("\033[1;33;40mFrom: ", sender)
+        print("Message: ", body)
+        print('\n\n---------------------------------------------------------------------\n\n')
 
-          # Now, the data obtained is in lxml. So, we will parse
-          # it with BeautifulSoup library
-          soup = BeautifulSoup(decoded_data , "lxml")
-          body = soup.body()
-
-          # Printing the subject, sender's email and message
-          print("\033[1;33;40mSubject: ", subject)
-          print("\033[1;32mFrom: ", sender)
-          print("Message: ", body)
-          print('\n\n---------------------------------------------------------------------\n\n')
-
-      except:
-          pass
+    except:
+        pass
 
 def threads():
   """Display threads with long conversations(>= 3 messages)
@@ -163,4 +160,4 @@ def main():
         print("Wrong input.")
 
 if __name__ == "__main__":
-  main()
+    main()
