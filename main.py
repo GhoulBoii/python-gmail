63dd4c65d7e5579407c13e555b9e07cf7766ebd5
GhoulBoii <github@ghoulcloud.slmail.me>
2024-05-29 16:27:24 +0530

    fix(): renaming to main and actually adding new scopes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.send",
     "https://www.googleapis.com/auth/gmail.readonly",
-    "https://www.googleapis.com/auth/gmail.modify"
+    "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/gmail.labels",
 ]
 
